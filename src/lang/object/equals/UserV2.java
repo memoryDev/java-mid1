d0f50275021a878e1ad178a9b240e7c55550c542
memoryDev <godot4027@kakao.com>
2024-05-05 14:42:14 +0900

    인프런 - 실전 자바 중급1편 - Equals 및 문제풀이

---
+++ src/lang/object/equals/UserV2.java
@@ -0,0 +1,27 @@
+package lang.object.equals;
+
+import java.util.Objects;
+
+public class UserV2 {
+    private String id;
+
+    public UserV2(String id) {
+        this.id = id;
+    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        UserV2 user = user = (UserV2)obj;
+//        return id.equals(user.id);
+//    }
+
+
+    // IDE 자동생성 해줌 외우기X
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserV2 userV2 = (UserV2) o;
+        return Objects.equals(id, userV2.id);
+    }
+}
